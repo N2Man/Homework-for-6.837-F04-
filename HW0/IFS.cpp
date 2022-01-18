caabddc463bc0a85ce06ed44a6396898529aae32
N2Man <490625185@qq.com>
2022-01-18 16:05:15 +0800

    Update IFS.cpp

--- HW0/IFS.cpp
+++ HW0/IFS.cpp
@@ -19,12 +19,10 @@ void IFS::readFile(char* filename) {
 	//FILE* file = fopen(filename, "r");
 	assert(file != NULL);
 	n = this->readFloat(file);
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		float p = this->readFloat(file);
 		Matrix m;
 		m.Read3x3(file);
-
 		this->probability.push_back(p);
 		this->transformation.push_back(m);
 	}
@@ -36,16 +34,13 @@ void IFS::render(Image& image,float num_points, float num_iters) {
 	int width = image.Width();
 	int height = image.Height();
 
-	for (int i = 0; i < num_points; ++i)
-	{
+	for (int i = 0; i < num_points; ++i) {
 		Vec2f v = Vec2f(rand() * 1.0f / RAND_MAX, rand() * 1.0f / RAND_MAX);
-		for (int j = 0; j < num_iters; j++)
-		{
+		for (int j = 0; j < num_iters; j++) {
 			int k = 0;
 			float sum = 0;
 			float t = rand() * 1.0f / RAND_MAX;
-			for (k = 0; k < n; k++)
-			{
+			for (k = 0; k < n; k++) {
 				sum += this->probability[k];
 				if (sum >= t)
 					break;
@@ -54,8 +49,7 @@ void IFS::render(Image& image,float num_points, float num_iters) {
 			}
 			this->transformation[k].Transform(v);
 		}
-		if ((v.x() >= 0 && v.x() <= 1) && (v.y() >= 0 && v.y() <= 1)) 
-		{
+		if ((v.x() >= 0 && v.x() <= 1) && (v.y() >= 0 && v.y() <= 1)) {
 			image.SetPixel(v.x() * width, v.y() * height, color);
 		}
 	}
